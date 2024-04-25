ebcb4246dcf74fd94f9560aa8470ffc2bde4de8e
Danil Druz FB-92 <91198860+DruzDanil@users.noreply.github.com>
2024-04-25 22:29:07 +0300

    Create messageserv.py

---
+++ Lab1/messageserv.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify, request
+import requests
+
+app = Flask(__name__)
+facade_server_url = 'http://127.0.0.1:5000'
+
+@app.route('/data', methods=['GET'])
+def data():
+    # Обробка GET запиту
+    return "not implemented yet"
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5002)
+
+
