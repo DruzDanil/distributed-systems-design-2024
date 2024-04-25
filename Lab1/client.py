ffe6b19e63eb49ef6f839749a8fbb028232d66bf
Danil Druz FB-92 <91198860+DruzDanil@users.noreply.github.com>
2024-04-25 22:28:59 +0300

    Create client.py

---
+++ Lab1/client.py
@@ -0,0 +1,13 @@
+import requests
+
+
+# Відправка POST запиту
+data = "Hello World"
+response_post = requests.post('http://127.0.0.1:5000/data', data=data)
+print('POST відповідь:', response_post.text)
+
+# Відправка GET запиту
+response_get = requests.get('http://127.0.0.1:5000/data')
+print('GET відповідь:', response_get.json())
+
+
