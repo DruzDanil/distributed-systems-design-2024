4b7aaa60f9af00a5beb1043e00dee9e5eb778c61
Danil Druz FB-92 <91198860+DruzDanil@users.noreply.github.com>
2024-05-14 18:19:49 +0300

    Create client.py

---
+++ Lab5/client.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+# Відправка POST запиту
+for i in range(10):
+    data = f"msg{i+1}"
+    response_post = requests.post('http://127.0.0.1:5000/data', data=data)
+    print('POST відповідь:', response_post.text)
+
+# Відправка GET запиту
+response_get = requests.get('http://127.0.0.1:5000/data').json()
+print('GET відповідь:', json.dumps(response_get, indent=5))
+response_get = requests.get('http://127.0.0.1:5000/data').json()
+print('GET відповідь:', json.dumps(response_get, indent=5))
